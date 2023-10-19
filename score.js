bd739499ba9585e0a28fa50e0e6c70c0c7d0a608
sak23042006 <s.arunkumar567567567@gmail.com>
2023-10-19 18:22:27 +0530

    commited

--- score.js
+++ score.js
@@ -1,1 +1,15 @@
 // Iteration 5: Store the player score and display it on the game over screen
+
+var Score =  document.getElementById("score-board")
+
+var value = localStorage.getItem("score")
+
+Score.textContent=value ;
+
+var again = document.getElementById("play-again-button")
+
+again.addEventListener("click",(e => {
+
+    location.href="game.html"
+
+}))
